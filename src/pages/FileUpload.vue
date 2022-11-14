58925585316a3863f60fd3d8cb3ad951eca07b79
LRA <1050685865@qq.com>
2022-11-14 10:26:14 +0800

    修改第二次

--- src/pages/FileUpload.vue
+++ src/pages/FileUpload.vue
@@ -1,40 +1,56 @@
 <template>
   <div>
     <h4>文件上传</h4>
-    <el-form>
+    <!-- <el-form
+      action="http://localhost:8080/addFile2"
+      method="post"
+      enctype="multipart/form-data"
+    >
       <el-form-item label="上传人姓名:">
-        <el-input placeholder="姓名"  v-model="cookiename"/>
+        <el-input placeholder="姓名" v-model="cookiename" name="user_name" />
       </el-form-item>
-    
-      <el-upload
+      <el-form-item label=" 选择文件:">
+        <input type="file" name="file" />
+      </el-form-item>
+      <br />
+      <el-form-item label="文件描述:">
+        <textarea rows="3" name="name"></textarea><br />
+      </el-form-item> -->
+    <!-- <el-upload
         class="upload-demo"
         ref="upload"
-        action="http://localhost:8080/upload"
         multiple
         :limit="5"
         :on-preview="handlePreview"
         :on-remove="handleRemove"
         :file-list="fileList"
         :auto-upload="false"
+        name="file"
       >
         <el-button slot="trigger" size="small" type="primary"
           >选取文件</el-button
-        >
-      </el-upload>
-      <br />
-      <el-form-item>
-        <el-button type="success" @click="submitUpload">确定</el-button>
+        > -->
+    <!-- </el-upload> -->
+    <br />
+    <!-- <el-input type="textarea" :rows="2" placeholder="请输入内容" name="name">
+      </el-input> -->
+    <!-- <el-form-item>
+        <el-button typr="primary" @click="submitUpload">确定</el-button>
       </el-form-item>
-    </el-form>
+    </el-form> -->
     <form
       action="http://localhost:8080/addFile2"
       method="post"
       enctype="multipart/form-data"
+      class="STYLE-NAME"
     >
-      姓名：<input type="text" name="user_name" v-model="cookiename" />
+      姓名：<input id="name" type="text" name="user_name" v-model="cookiename" />
+      <br />
       <br />
       选择文件:<input type="file" name="file" /><br />
-      文件描述：<textarea rows="3" name="name"></textarea><br />
+      <br />
+      文件描述：<textarea id="message" rows="3" name="name"></textarea><br />
+      <br />
       <input type="submit" value="提交" @click="upload" />
     </form>
   </div>
@@ -60,7 +76,8 @@ export default {
   },
   methods: {
     submitUpload() {
-      this.$refs.upload.submit();
+      this.$cookies.get("user_name");
+      // this.$refs.upload.submit();
       // this.$cookieStore.getCookie( 'name')
       // this.insert.id=nanoid();
 
@@ -76,9 +93,7 @@ export default {
       //     console.log(res);
       //   });
     },
-    upload() {
-      this.$cookies.get("user_name");
-    },
+    upload() {},
     handleRemove(file, fileList) {
       console.log(file, fileList);
     },
